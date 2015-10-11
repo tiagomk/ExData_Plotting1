1618f4c54bc73b371a74a83a2d032ab3119fd111
Tiago Moura Kohmann <tiagomk@yahoo.com.br>
2015-10-11 14:37:13 -0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,5 @@
+# Start of Script
+
 # Download data file
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
@@ -89,3 +91,5 @@ axis(2, at=c(0, 10, 20, 30))
 
 # Turn plot device off
 dev.off()
+
+# End of Script
